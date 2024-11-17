8c2aee95cba1cd643d68835ab7d7d213227b62e4
Sankalp Sangle <sankalpsangle98@gmail.com>
2024-11-17 02:00:03 -0500

    Add relative line numbers for neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- relative line numbers
+vim.wo.number = true -- Show current line number
+vim.wo.relativenumber = true -- Show relative line numbers
